4bedecf3a720a47884fe9d02712afeb4582bc7ac
Joe <joe.keefe@gmail.com>
2024-03-20 01:13:23 -0400

    Initial commit for hackrf one spectrum analyzer

---
+++ spectrum_analyzer.py
@@ -0,0 +1,226 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+#
+# SPDX-License-Identifier: GPL-3.0
+#
+# GNU Radio Python Flow Graph
+# Title: spectrum_analyzer
+# Author: JK
+# GNU Radio version: 3.10.7.0
+
+from packaging.version import Version as StrictVersion
+from PyQt5 import Qt
+from gnuradio import qtgui
+from gnuradio import gr
+from gnuradio.filter import firdes
+from gnuradio.fft import window
+import sys
+import signal
+from PyQt5 import Qt
+from argparse import ArgumentParser
+from gnuradio.eng_arg import eng_float, intx
+from gnuradio import eng_notation
+from gnuradio import soapy
+from gnuradio.qtgui import Range, RangeWidget
+from PyQt5 import QtCore
+import sip
+import time, threading
+
+
+
+class spectrum_analyzer(gr.top_block, Qt.QWidget):
+
+    def __init__(self):
+        gr.top_block.__init__(self, "spectrum_analyzer", catch_exceptions=True)
+        Qt.QWidget.__init__(self)
+        self.setWindowTitle("spectrum_analyzer")
+        qtgui.util.check_set_qss()
+        try:
+            self.setWindowIcon(Qt.QIcon.fromTheme('gnuradio-grc'))
+        except BaseException as exc:
+            print(f"Qt GUI: Could not set Icon: {str(exc)}", file=sys.stderr)
+        self.top_scroll_layout = Qt.QVBoxLayout()
+        self.setLayout(self.top_scroll_layout)
+        self.top_scroll = Qt.QScrollArea()
+        self.top_scroll.setFrameStyle(Qt.QFrame.NoFrame)
+        self.top_scroll_layout.addWidget(self.top_scroll)
+        self.top_scroll.setWidgetResizable(True)
+        self.top_widget = Qt.QWidget()
+        self.top_scroll.setWidget(self.top_widget)
+        self.top_layout = Qt.QVBoxLayout(self.top_widget)
+        self.top_grid_layout = Qt.QGridLayout()
+        self.top_layout.addLayout(self.top_grid_layout)
+
+        self.settings = Qt.QSettings("GNU Radio", "spectrum_analyzer")
+
+        try:
+            if StrictVersion(Qt.qVersion()) < StrictVersion("5.0.0"):
+                self.restoreGeometry(self.settings.value("geometry").toByteArray())
+            else:
+                self.restoreGeometry(self.settings.value("geometry"))
+        except BaseException as exc:
+            print(f"Qt GUI: Could not restore geometry: {str(exc)}", file=sys.stderr)
+
+        ##################################################
+        # Variables
+        ##################################################
+        self.tuning = tuning = 2400000000
+        self.sample_rate = sample_rate = 1000000
+        self.samp_rate = samp_rate = 1000000
+        self.if_gain = if_gain = 7
+        self.gain = gain = 15
+        self.bandwidth = bandwidth = 10000000
+
+        ##################################################
+        # Blocks
+        ##################################################
+
+        self._tuning_range = Range(1000000, 6000000000, 1000, 2400000000, 200)
+        self._tuning_win = RangeWidget(self._tuning_range, self.set_tuning, "Frequency", "counter_slider", float, QtCore.Qt.Horizontal)
+        self.top_layout.addWidget(self._tuning_win)
+        self._if_gain_range = Range(0, 40, 1, 7, 200)
+        self._if_gain_win = RangeWidget(self._if_gain_range, self.set_if_gain, "IF Gain", "counter_slider", float, QtCore.Qt.Horizontal)
+        self.top_layout.addWidget(self._if_gain_win)
+        self._gain_range = Range(0, 62, 1, 15, 200)
+        self._gain_win = RangeWidget(self._gain_range, self.set_gain, "VGA Gain", "counter_slider", float, QtCore.Qt.Horizontal)
+        self.top_layout.addWidget(self._gain_win)
+        self._bandwidth_range = Range(1000, 10000000, 1000, 10000000, 200)
+        self._bandwidth_win = RangeWidget(self._bandwidth_range, self.set_bandwidth, "Bandwidth", "counter_slider", float, QtCore.Qt.Horizontal)
+        self.top_layout.addWidget(self._bandwidth_win)
+        self.soapy_hackrf_source_0 = None
+        dev = 'driver=hackrf'
+        stream_args = ''
+        tune_args = ['']
+        settings = ['']
+
+        self.soapy_hackrf_source_0 = soapy.source(dev, "fc32", 1, '',
+                                  stream_args, tune_args, settings)
+        self.soapy_hackrf_source_0.set_sample_rate(0, samp_rate)
+        self.soapy_hackrf_source_0.set_bandwidth(0, bandwidth)
+        self.soapy_hackrf_source_0.set_frequency(0, tuning)
+        self.soapy_hackrf_source_0.set_gain(0, 'AMP', False)
+        self.soapy_hackrf_source_0.set_gain(0, 'LNA', min(max(if_gain, 0.0), 40.0))
+        self.soapy_hackrf_source_0.set_gain(0, 'VGA', min(max(gain, 0.0), 62.0))
+        self._sample_rate_range = Range(1000000, 20000000, 1000000, 1000000, 200)
+        self._sample_rate_win = RangeWidget(self._sample_rate_range, self.set_sample_rate, "Sample Rate", "counter_slider", float, QtCore.Qt.Horizontal)
+        self.top_layout.addWidget(self._sample_rate_win)
+        self.qtgui_sink_x_0 = qtgui.sink_c(
+            4096, #fftsize
+            window.WIN_HAMMING, #wintype
+            tuning, #fc
+            samp_rate, #bw
+            "Spectrum Analyzer", #name
+            True, #plotfreq
+            True, #plotwaterfall
+            True, #plottime
+            True, #plotconst
+            None # parent
+        )
+        self.qtgui_sink_x_0.set_update_time(1.0/20)
+        self._qtgui_sink_x_0_win = sip.wrapinstance(self.qtgui_sink_x_0.qwidget(), Qt.QWidget)
+
+        self.qtgui_sink_x_0.enable_rf_freq(True)
+
+        self.top_layout.addWidget(self._qtgui_sink_x_0_win)
+
+
+        ##################################################
+        # Connections
+        ##################################################
+        self.msg_connect((self.qtgui_sink_x_0, 'freq'), (self.soapy_hackrf_source_0, 'cmd'))
+        self.connect((self.soapy_hackrf_source_0, 0), (self.qtgui_sink_x_0, 0))
+
+
+    def closeEvent(self, event):
+        self.settings = Qt.QSettings("GNU Radio", "spectrum_analyzer")
+        self.settings.setValue("geometry", self.saveGeometry())
+        self.stop()
+        self.wait()
+
+        event.accept()
+
+    def get_tuning(self):
+        return self.tuning
+
+    def set_tuning(self, tuning):
+        self.tuning = tuning
+        self.qtgui_sink_x_0.set_frequency_range(self.tuning, self.samp_rate)
+        self.soapy_hackrf_source_0.set_frequency(0, self.tuning)
+
+    def get_sample_rate(self):
+        return self.sample_rate
+
+    def set_sample_rate(self, sample_rate):
+        self.sample_rate = sample_rate
+
+    def get_samp_rate(self):
+        return self.samp_rate
+
+    def set_samp_rate(self, samp_rate):
+        self.samp_rate = samp_rate
+        self.qtgui_sink_x_0.set_frequency_range(self.tuning, self.samp_rate)
+        self.soapy_hackrf_source_0.set_sample_rate(0, self.samp_rate)
+
+    def get_if_gain(self):
+        return self.if_gain
+
+    def set_if_gain(self, if_gain):
+        self.if_gain = if_gain
+        self.soapy_hackrf_source_0.set_gain(0, 'LNA', min(max(self.if_gain, 0.0), 40.0))
+
+    def get_gain(self):
+        return self.gain
+
+    def set_gain(self, gain):
+        self.gain = gain
+        self.soapy_hackrf_source_0.set_gain(0, 'VGA', min(max(self.gain, 0.0), 62.0))
+
+    def get_bandwidth(self):
+        return self.bandwidth
+
+    def set_bandwidth(self, bandwidth):
+        self.bandwidth = bandwidth
+        self.soapy_hackrf_source_0.set_bandwidth(0, self.bandwidth)
+
+
+def foo(tb):
+    print(time.ctime())
+    center_freq = tb.get_tuning()
+    center_freq += 1000000
+    tb.set_tuning(center_freq)
+    threading.Timer(2, foo).start()
+
+
+def main(top_block_cls=spectrum_analyzer, options=None):
+
+    if StrictVersion("4.5.0") <= StrictVersion(Qt.qVersion()) < StrictVersion("5.0.0"):
+        style = gr.prefs().get_string('qtgui', 'style', 'raster')
+        Qt.QApplication.setGraphicsSystem(style)
+    qapp = Qt.QApplication(sys.argv)
+
+    tb = top_block_cls()
+
+    tb.start(1)
+
+    tb.show()
+
+    def sig_handler(sig=None, frame=None):
+        tb.stop()
+        tb.wait()
+
+        Qt.QApplication.quit()
+
+    signal.signal(signal.SIGINT, sig_handler)
+    signal.signal(signal.SIGTERM, sig_handler)
+
+    timer = Qt.QTimer()
+    timer.start(500)
+    timer.timeout.connect(lambda: None)
+
+    foo(tb)
+
+    qapp.exec_()
+
+if __name__ == '__main__':
+    main()
